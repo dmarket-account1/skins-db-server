d1df71a4dc8424ba7ee818f2466f4b0da08f31d6
Andrew <andriyilko@gmail.com>
2023-06-11 01:47:46 +0300

    Update

--- index.js
+++ index.js
@@ -13,43 +13,31 @@ const MONGODB_URL = process.env.MONGODB_URL;
 
 const mongoClient = new mongodb.MongoClient(MONGODB_URL);
 
-let data = {};
-
 const updateSkinsDB = async () => {
   const db = mongoClient.db("steam-skins");
   data = { ...db.collection("items-db").findOne({ id: "items-db" }) };
   delete data._id;
   delete data.id;
-};
-
-const start = () => {
-  updateSkinsDB();
-
-  setInterval(() => {
-    updateSkinsDB;
-  }, 21600 * 1000);
+  return data;
 };
 
 app.get("/", (req, res) => {
   res.status(200).json({
     status: "SkinsDB is working.",
-    info: {
-      "last_updated": data.lastUpdated,
-    },
   });
 });
 
-app.get("/db", (req, res) => {
-  if (data.data) {
-    res.status(200).json(data.data);
-  } else {
-    res.status(400).send("Try one more time");
+app.get("/db", async (req, res) => {
+  try {
+    const data = await updateSkinsDB();
+    res.status(200).json(data);
+  } catch (e) {
+    res.status(400).send("Try again later.");
   }
 });
 
 app.listen(3000, async () => {
   mongoClient.connect().then(() => {
     console.log(`Server is live.`);
-    start();
   });
 });
