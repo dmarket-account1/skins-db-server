2c7570b105bc9a0293048abad05fdfed365665af
Andrew <andriyilko@gmail.com>
2023-06-17 00:00:06 +0300

    Add endpoint for skin creation

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { query } from "express";
 import cors from "cors";
 import * as mongodb from "mongodb";
 import dotenv from "dotenv";
@@ -10,6 +10,7 @@ app.use(express.json({ limit: "10mb" }));
 dotenv.config();
 
 const MONGODB_URL = process.env.MONGODB_URL;
+const JWT_TOKEN = process.env.JWT_TOKEN;
 
 const mongoClient = new mongodb.MongoClient(MONGODB_URL);
 
@@ -37,6 +38,44 @@ app.get("/db", async (req, res) => {
     console.log(error);
     res.status(400).send("Try again later.");
   }
+});
+
+app.post("/add-skin", async (req, res) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    res.status(400).send("Bad request.");
+    return;
+  }
+  try {
+    const token = authHeader.split(" ")[1];
+    if (token !== JWT_TOKEN) {
+      throw new Error("Try again later.");
+    }
+  } catch (error) {
+    res.status(401).send("Unauthorized.");
+    return;
+  }
+  const body = req.body;
+
+  if (body.data == undefined) {
+    res.status(400).send("Bad request.");
+    return;
+  }
+
+  const db = mongoClient.db("steam-skins");
+  body.data.date = new Date(body.data.date);
+  await db.collection("skins").insertOne(body.data);
+  res.status(200).send("Success");
+});
+
+app.get("/skins", async (req, res) => {
+  const query = req.query;
+  const filter = {};
+
+  const db = mongoClient.db("steam-skins");
+  const skins = await db.collection("skins").find({}).toArray();
+
+  res.status(200).json({ data: skins });
 });
 
 app.listen(3000, async () => {
