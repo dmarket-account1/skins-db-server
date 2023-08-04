eb13a1e9f142edee696d633b209d8faf23068f7e
Andrew <andriyilko@gmail.com>
2023-08-04 10:19:24 +0300

    Update

--- index.js
+++ index.js
@@ -172,6 +172,41 @@ app.get("/buff/min-order", async (req, res) => {
   res.json({ data: skins });
 });
 
+app.post("/add", async (req, res) => {
+  const body = req.body;
+
+  if (body.data == undefined) {
+    res.status(400).send("Bad request.");
+    return;
+  }
+
+  const db = mongoClient.db("steam-skins");
+  const isSkin = await db.collection("skins").findOne({
+    "extra.floatValue": body.data.extra.floatValue,
+  });
+
+  if (isSkin) {
+    if (
+      body.data.status.includes("TxSuccess") &&
+      isSkin.status.includes("TxFailed")
+    ) {
+      await db
+        .collection("skins")
+        .findOneAndUpdate(
+          { "extra.floatValue": body.data.extra.floatValue },
+          { $set: body.data }
+        );
+      res.status(200).send("Success");
+    }
+    res.status(400).send("Skin has been already added");
+    return;
+  }
+
+  body.data.date = new Date(body.data.date);
+  await db.collection("skins").insertOne(body.data);
+  res.status(200).send("Success");
+});
+
 app.listen(8888, async () => {
   mongoClient.connect().then(() => {
     console.log(`Server is live.`);
